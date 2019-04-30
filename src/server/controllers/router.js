6442905bafbc109365a351fa4501faf41b2f320d
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-30 12:24:56 +0300

    Handled the default route with login.login handler

--- src/server/controllers/router.js
+++ src/server/controllers/router.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
 
-const error = require('./error');
+const error = require("./error");
+const login = require("./loginPage");
 
 // router.get('/fruit', fruit.get);
 // router.post('/fruit', fruit.post);
-router.get("/",(req,res)=>{
-  var x = path.join(__dirname,"..","..","..","public","layouts","index.html")
- res.sendFile(x)
-})
-
+router.get("/", (req, res) => login.login(res));
 
 router.use(error.client);
 router.use(error.server);
