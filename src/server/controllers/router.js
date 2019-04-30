9c7ad44f2f05ed734ead90640e1900da34b31c97
mrfong <therealshadix@gmail.com>
2019-04-30 11:15:22 +0300

    added routing for the main directory, send the main index.html file

--- src/server/controllers/router.js
+++ src/server/controllers/router.js
@@ -6,6 +6,12 @@ const error = require('./error');
 
 // router.get('/fruit', fruit.get);
 // router.post('/fruit', fruit.post);
+router.get("/",(req,res)=>{
+  var x = path.join(__dirname,"..","..","..","public","layouts","index.html")
+ res.sendFile(x)
+})
+
+
 router.use(error.client);
 router.use(error.server);
 
