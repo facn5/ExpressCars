9e17a7630440885bd3e644ecb1584132779008b5
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-30 16:24:02 +0300

    Added route for /login

--- src/server/controllers/router.js
+++ src/server/controllers/router.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
-const router = express.Router();
 
+const router = express.Router();
 const error = require("./error");
 const login = require("./loginPage");
 
 // router.get('/fruit', fruit.get);
 // router.post('/fruit', fruit.post);
-router.get("/", (req, res) => login.login(res));
+
+
+router.post("/login", ({ body }, res) => {
+  login.login(res, body.username, body.password);
+});
 
 router.use(error.client);
-router.use(error.server);
-
+// router.use(error.server);
 module.exports = router;
