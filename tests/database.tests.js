8e25cb6501d0b5184533798c633e2a34ea95ba37
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-01 14:49:38 +0300

    Added test for creating account and hashing the password in the database

--- tests/database.tests.js
+++ tests/database.tests.js
@@ -1,15 +1,42 @@
 const supertest = require("supertest");
 const tape = require("tape");
-const sql = require('src/database/queries/sql');
+const sql = require("../src/database/queries/sql");
+const utils = require("../src/utils/utils");
 
-tape("homepage endpoint", t => {
-  supertest(app)
-    .get("/")
-    .expect(200)
-    .end(function(err, res) {
-      t.equal(res.statusCode, 302, `/ route redirects you to the home route`);
-      t.end();
-    });
+const newUserToCheck = "faris1145";
+const newPasswordToCheck = "faris1145Password";
+
+tape("Creating account", t => {
+  utils.hashPassword(newPasswordToCheck, (err, hashedPassword) => {
+    if (err) t.end("Error while hashing the password");
+    else {
+      sql.createAccount(newUserToCheck, hashedPassword, (err, result) => {
+        if (err) t.end("Error, account not created successufully");
+        else {
+          sql.getUsernamePassword(newUserToCheck, (err, result) => {
+            if (err) t.end("Error, getting the password from the database");
+            else {
+              utils.comparePasswords(
+                newPasswordToCheck,
+                result.rows[0].password,
+                (err, success) => {
+                  if (err) t.end(err);
+                  else {
+                    t.equal(
+                      success,
+                      true,
+                      "Hashed pass in db and should be equal"
+                    );
+                    t.end();
+                  }
+                }
+              );
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 tape.onFinish(() => process.exit(0));
