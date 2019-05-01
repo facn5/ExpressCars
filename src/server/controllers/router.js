392a98d273e853f759906725757a36d76efaaca7
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-01 11:50:34 +0300

    Added route of /register

--- src/server/controllers/router.js
+++ src/server/controllers/router.js
@@ -4,6 +4,7 @@ const path = require("path");
 const router = express.Router();
 const error = require("./error");
 const login = require("./loginPage");
+const register = require("./registerPage");
 
 // router.get('/fruit', fruit.get);
 // router.post('/fruit', fruit.post);
@@ -11,14 +12,20 @@ router.get("/", (req, res) => {
   res.redirect('/home');
 })
 
+router.get("/register??", (req, res) => {
+  register.page(res);
+})
+
+
 router.get("/home", (req, res) => {
   login.checkauth(res, req);
 });
+
 
 router.post("/login", ({ body }, res) => {
   login.login(res, body.username, body.password);
 });
 
 router.use(error.client);
-// router.use(error.server);
+router.use(error.server);
 module.exports = router;
