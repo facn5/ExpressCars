f62e9314b7aa01dfee6f0f100fce73980ae40b79
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-01 12:10:25 +0300

    Added a query to check if username already exists

--- src/database/queries/sql.js
+++ src/database/queries/sql.js
@@ -7,11 +7,24 @@ const getPasswordWhereUsernameEqualsArg1 = (username, cb) =>
     cb
   );
 
-const getCars = (cb,manufacturer,type,price) =>
-query.insert("SELECT * from cars where manufacturer = $1 and type = $2 and price = $3 ",[manufacturer,type,price] ,cb);
+const getCars = (cb, manufacturer, type, price) =>
+  query.insert(
+    "SELECT * from cars where manufacturer = $1 and type = $2 and price = $3 ",
+    [manufacturer, type, price],
+    cb
+  );
+
+const getUsernameWhereUsernameEqualsArg1 = (username, cb) =>
+  query.insert(
+    `SELECT username from users where username = $1;`,
+    [username],
+    cb
+  );
 
 
 module.exports = {
-  getCars:getCars,
+  getCars: getCars,
+  createAccount: createAccount,
+  getUsernameUsername: getUsernameWhereUsernameEqualsArg1,
   getUsernamePassword: getPasswordWhereUsernameEqualsArg1
 };
