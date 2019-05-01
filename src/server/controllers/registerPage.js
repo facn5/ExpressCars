f660203e2c0a19c7656f57f38af789f46c8e603f
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-01 12:12:51 +0300

    Added function for creating accounts and sending cookies, if created succesfully redirect to home page

--- src/server/controllers/registerPage.js
+++ src/server/controllers/registerPage.js
@@ -1,3 +1,7 @@
+const sql = require("../../database/queries/sql");
+const { sign, verify } = require("jsonwebtoken");
+const ppcookie = require("cookie");
+const utils = require("../../utils/utils");
 
 exports.page = res => {
   const endObject = {
@@ -7,6 +11,59 @@ exports.page = res => {
   res.render("layouts/registerPageLayout", endObject);
 };
 
-exports.register = (req, res) => {
+exports.reg = (user, pass, res) => {
+  if (user != null && pass != null) {
+    sql.getUsernameUsername(user, (err, result) => {
+      if (err)
+        res.render("layouts/registerPageLayout", {
+          msg: "Please try again later!",
+          color: "red"
+        });
+      else {
+        if (result) {
+          if (result.rowCount != 0)
+            res.render("layouts/registerPageLayout", {
+              msg: "Username already exists!",
+              color: "red"
+            });
+          else {
+            utils.hashPassword(pass, (err, hashedPass) => {
+              if (err)
+                res.render("layouts/registerPageLayout", {
+                  msg: "Please try again later!",
+                  color: "red"
+                });
+              else {
+                sql.createAccount(user, hashedPass, (err, success) => {
+                  if (err) console.log(err);
+                  else {
+                    if (success) {
+                      const userDetails = {
+                        "content-type": "application/json",
+                        u$u: user,
+                        u$p: pass
+                      };
 
-}
+                      const cookie = sign(userDetails, process.env.SECRET);
+
+                      let options = {
+                        httpOnly: true
+                      };
+
+                      res.cookie("udetails", cookie, options);
+                      res.redirect('/home')
+                    }
+                  }
+                });
+              }
+            });
+          }
+        } else
+          res.render("layouts/registerPageLayout", {
+            msg: "Please try again later!",
+            color: "red"
+          });
+      }
+    });
+  }
+};
