3303ce260b1aa9cf71210ab096bb873b8ec53dc8
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-01 12:11:48 +0300

    404 error renders 404 layout hbs

--- src/server/controllers/error.js
+++ src/server/controllers/error.js
@@ -1,17 +1,5 @@
 const path = require("path");
 
 exports.client = (req, res) => {
-  res
-    .status(404)
-    .sendFile(
-      path.join(__dirname, "..", "..", "..", "public", "layouts", "404.html")
-    );
-};
-
-exports.server = (err, req, res, next) => {
-  res
-    .status(500)
-    .sendFile(
-      path.join(__dirname, "..", "..", "..", "public", "layouts", "500.html")
-    );
+  res.render('layouts/404');
 };
