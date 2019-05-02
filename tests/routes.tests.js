7c6448aed3e6da0876b8955222d93343e053369c
faris114 <farisabutaya@gmail.com>
2019-05-02 12:11:52 +0300

    test modified so tests for status code done once Relates 73#

--- tests/routes.tests.js
+++ tests/routes.tests.js
@@ -5,7 +5,6 @@ const app = require("../src/server/app");
 tape("homepage endpoint", t => {
   supertest(app)
     .get("/")
-    .expect(200)
     .end(function(err, res) {
       t.equal(res.statusCode, 302, `/ route redirects you to the home route`);
       t.end();
@@ -15,7 +14,6 @@ tape("homepage endpoint", t => {
 tape("homepage endpoint", t => {
   supertest(app)
     .get("/home")
-    .expect(200)
     .end(function(err, res) {
       t.equal(res.statusCode, 200, `homepage endpoint redirects the user when he is not authenticated`);
       t.end();
@@ -25,7 +23,6 @@ tape("homepage endpoint", t => {
 tape("homepage endpoint", t => {
   supertest(app)
     .get("/hdasadssda")
-    .expect(404)
     .end(function(err, res) {
       t.equal(res.statusCode, 404, `weird endpoint return 404 status code`);
       t.end();
