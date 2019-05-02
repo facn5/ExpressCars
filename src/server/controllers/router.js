b36bb0058083648194fe404a224a0a010d1bc5e2
karam1ashqar <karam.ashqar01@gmail.com>
2019-05-02 12:03:03 +0300

    Sent the whole req as argument not only req.body

--- src/server/controllers/router.js
+++ src/server/controllers/router.js
@@ -5,19 +5,19 @@ const router = express.Router();
 const error = require("./error");
 const login = require("./loginPage");
 const register = require("./registerPage");
-const search = require("./searchCars")
+const search = require("./searchCars");
 
 // router.get('/fruit', fruit.get);
 // router.post('/fruit', fruit.post);
 router.get("/", (req, res) => {
-  res.redirect('/home');
-})
+  res.redirect("/home");
+});
 
 router.get("/register??", (req, res) => {
   register.page(res);
-})
+});
 
-router.post('/reg', ( { body }, res) => {
+router.post("/reg", ({ body }, res) => {
   register.reg(body.username, body.password, res);
 });
 
@@ -25,15 +25,13 @@ router.get("/home", (req, res) => {
   login.checkauth(res, req);
 });
 
-
 router.post("/login", ({ body }, res) => {
   login.login(res, body.username, body.password);
 });
 
-router.post("/search",({body},res)=>{
-
-search.search(res,body.name,body.origin)
-})
+router.post("/search", (req, res) => {
+  search.search(req, res, req.body.name, req.body.origin);
+});
 router.use(error.client);
 // router.use(error.server);
 module.exports = router;
